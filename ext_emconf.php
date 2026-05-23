37605c6734c1778420c2200b76da6d98402e96c9
Tanel Põld <tanel@brightside.ee>
2026-05-23 09:51:02 +0300

    [RLEASE] 0.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Transliterate Slugger',
-    'description' => 'Native PHP ICU transliteration for TYPO3 slug fields.',
+    'description' => 'Native PHP ICU transliteration for TYPO3 slug fields and file names.',
     'category' => 'be',
     'author' => 'Tanel Põld',
     'author_email' => 'tanel@brightside.ee',
     'author_company' => 'Brightside OÜ / t3brightside.com',
     'state' => 'stable',
-    'version' => '0.2.2',
+    'version' => '0.3.0',
     'constraints' => [
         'depends' => [
             'typo3' => '14.0.0 - 14.9.99',
